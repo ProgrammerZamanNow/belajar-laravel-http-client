b4b1e47bb04292b3f5141732a871f8a9c4c21096
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-01-07 11:44:38 +0700

    Add HttpTest.php for testing HTTP methods
    This commit introduces a new test file, HttpTest.php, which contains unit tests for testing various HTTP methods such as GET, POST, and DELETE. It also includes a test to assess HTTP response handling, specifically checking the response status code, headers, body, and JSON output.

---
+++ tests/Feature/HttpTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Http;
+use Tests\TestCase;
+
+class HttpTest extends TestCase
+{
+    public function testGet()
+    {
+        $response = Http::get("https://enhmm1ik062ud.x.pipedream.net");
+        self::assertTrue($response->ok());
+    }
+
+    public function testPost()
+    {
+        $response = Http::post("https://enhmm1ik062ud.x.pipedream.net");
+        self::assertTrue($response->ok());
+    }
+
+    public function testDelete()
+    {
+        $response = Http::delete("https://enhmm1ik062ud.x.pipedream.net");
+        self::assertTrue($response->ok());
+    }
+
+    public function testResponse()
+    {
+        $response = Http::get("https://enhmm1ik062ud.x.pipedream.net");
+        self::assertEquals(200, $response->status());
+        self::assertNotNull($response->headers());
+        self::assertNotNull($response->body());
+
+        $json = $response->json();
+        self::assertTrue($json['success']);
+    }
+
+
+}
